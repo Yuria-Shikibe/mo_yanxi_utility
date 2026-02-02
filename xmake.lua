bb80327cdf8e55ebd62f758fba42cb664896ed88
Yuria-Shikibe <3415339403@qq.com>
2026-02-02 15:07:40 +0800

    xmake update

--- xmake.lua
+++ xmake.lua
@@ -3,6 +3,41 @@ set_arch("x64")
 set_encodings("utf-8")
 set_project("mo_yanxi.utility")
 
+local available_components = {
+    "algo",
+    "concurrent",
+    "container",
+    "deprecated",
+    "dim2",
+    "general",
+    "generic",
+    "io",
+    "math",
+    "pointer",
+    "string",
+}
+
+function mo_yanxi_define_options()
+    for _, name in ipairs(available_components) do
+        option("with_" .. name)
+            set_default(true) -- 默认是否开启，可根据需求改为 false
+            set_showmenu(true)
+            set_description("Enable " .. name .. " component")
+        option_end()
+    end
+end
+
+-- 【新增】获取当前配置下启用了哪些组件
+function mo_yanxi_get_enabled_components()
+    local enabled = {}
+    for _, name in ipairs(available_components) do
+        if has_config("with_" .. name) then
+            table.insert(enabled, name)
+        end
+    end
+ret
+
+
 if is_plat("windows") then
     if is_mode("debug") then
         set_runtimes("MDd")
@@ -38,6 +73,5 @@ target("mo_yanxi.utility.test")
     add_files("test/**.cpp")
     add_deps("mo_yanxi.utility")
 target_end()
-
 
 includes("xmake2cmake.lua")
