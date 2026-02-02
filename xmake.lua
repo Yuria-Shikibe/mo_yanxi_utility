f03a26d6c6b694ad551b3cb94c9f0b97f6165c9d
Yuria-Shikibe <3415339403@qq.com>
2026-02-02 16:45:07 +0800

    clean up

--- xmake.lua
+++ xmake.lua
@@ -1,42 +1,7 @@
 add_rules("mode.debug", "mode.release")
 set_arch("x64")
 set_encodings("utf-8")
-set_project("mo_yanxi.react_flow")
-
-local available_components = {
-    "algo",
-    "concurrent",
-    "container",
-    "deprecated",
-    "dim2",
-    "general",
-    "generic",
-    "io",
-    "math",
-    "pointer",
-    "string",
-}
-
--- function mo_yanxi_define_options()
---     for _, name in ipairs(available_components) do
---         option("with_" .. name)
---             set_default(true) -- 默认是否开启，可根据需求改为 false
---             set_showmenu(true)
---             set_description("Enable " .. name .. " component")
---         option_end()
---     end
--- end
---
--- -- 【新增】获取当前配置下启用了哪些组件
--- function mo_yanxi_get_enabled_components()
---     local enabled = {}
---     for _, name in ipairs(available_components) do
---         if has_config("with_" .. name) then
---             table.insert(enabled, name)
---         end
---     end
--- ret
-
+set_project("mo_yanxi.utility")
 
 if is_plat("windows") then
     if is_mode("debug") then
@@ -50,16 +15,7 @@ end
 
 includes("config.lua");
 
-target("mo_yanxi.utility")
-    set_kind("object")
-    set_languages("c++23")
-    set_policy("build.c++.modules", true)
-
-    set_warnings("all")
-    set_warnings("pedantic")
-target_end()
-
-mo_yanxi_utility_add_comp_to("mo_yanxi.utility")
+mo_yanxi_utility_import_default("mo_yanxi.utility")
 
 target("mo_yanxi.utility.test")
     set_kind("binary")
@@ -73,5 +29,6 @@ target("mo_yanxi.utility.test")
     add_files("test/**.cpp")
     add_deps("mo_yanxi.utility")
 target_end()
+
 
 includes("xmake2cmake.lua")
