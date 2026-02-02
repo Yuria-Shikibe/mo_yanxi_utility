7345ffc075e3d27316b82c23cd54429716d61b6a
Yuria-Shikibe <3415339403@qq.com>
2026-02-02 16:44:13 +0800

    clean up

--- xmake.lua
+++ xmake.lua
@@ -1,7 +1,7 @@
 add_rules("mode.debug", "mode.release")
 set_arch("x64")
 set_encodings("utf-8")
-set_project("mo_yanxi.utility")
+set_project("mo_yanxi.react_flow")
 
 if is_plat("windows") then
     if is_mode("debug") then
